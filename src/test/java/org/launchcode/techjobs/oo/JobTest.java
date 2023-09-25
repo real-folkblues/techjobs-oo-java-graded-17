364c26afa38845d151f21993e45a252b52a1e250
real-folkblues <marenicolor@gmail.com>
2023-09-25 12:45:26 -0400

    troubleshot gradle, removed then rebuilt with junit5, writing 1st test, import bugs

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,6 +1,17 @@
 package org.launchcode.techjobs.oo;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JobTest {
-    @Test
+
     //TODO: Create your unit tests here
+    public void testSettingJobId() {
+        unexpected = new JobTest(int id);
+        actual = new JobTest()
+            AsserNotEquals(unexpected, job1);
+
+    }
+
 }
